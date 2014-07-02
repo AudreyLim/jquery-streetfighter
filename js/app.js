a02a347aad58d5b1fd7720f96afda2b5ea392680
Audrey <audreylh@gmail.com>
2014-07-02 20:45:03 +0800

    ryucool fixed

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+
 	$('.ryu').mouseenter(function(){
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
@@ -22,6 +24,32 @@ $(document).ready(function(){
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
+
+	$("body").keydown(function(e){
+		if(e.keyCode == 88){
+			$('.ryu-still').hide();
+			$('.ryu-cool').show(); 
+		}
+		})
+	.keyup(function(e){
+		if(e.keyCode == 88){
+			$('.ryu-still').show();
+			$('.ryu-cool').hide(); 
+		}
+		});
+
+
+	
+
+
+
+
+
+
+
+
+	
+	
 });
 
 function playHadouken () {
